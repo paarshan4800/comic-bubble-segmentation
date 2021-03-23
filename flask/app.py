4231ec92d9e44c3f0e9dc9de0284c797f2ce782c
akashkirthik <75619135+akashkirthik@users.noreply.github.com>
2021-03-23 19:18:13 +0530

    23-03 spot

--- flask/app.py
+++ flask/app.py
@@ -3,24 +3,15 @@ import base64
 import io
 import numpy
 import os
-
 from comic_book_reader import  findSpeechBubbles
 from flask import Flask, request
 
-
 application = Flask(__name__)
-
-
-
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 def allowed_file(filename):
-    
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-
 
 @application.route('/segment', methods=['POST'])
 def segment():
@@ -29,19 +20,19 @@ def segment():
         return 'No file sent', 400
 
     file = request.files['image']
-    
+
     if file and allowed_file(file.filename):
         npimg = numpy.fromstring(file.read(), numpy.uint8)
         img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+        
         contours = findSpeechBubbles(img)
+        print("\n\n\n\n")
         cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
         #cv2.imwrite('C:/Users/Akash/Desktop/Contours.jpg', img)
+
         _, buffer = cv2.imencode('.jpg', img)
-        
+
         return {"message":"20 percent done"}
-
-
-
 
 
 if __name__ == '__main__':
--- flask/comic_book_reader.py
+++ flask/comic_book_reader.py
@@ -1,81 +1,88 @@
-import cv2
+import cv2,time
 import os
 import matplotlib.pyplot as plt 
-
-
-
 
 def get_contour_precedence(contour, cols):
     tolerance_factor = 200
     origin = cv2.boundingRect(contour)
     return ((origin[1] // tolerance_factor) * tolerance_factor) * cols + origin[0]
 
-
-def findSpeechBubbles(image):
-    
+def findSpeechBubbles(image): 
+    printlist=[] 
+    #create gray image
     imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    
-    
-    
     #cv2.imwrite('C:/Users/Akash/Desktop/Test_gray.jpg', imageGray)
 
+    #show gray image
     plt.imshow(imageGray, cmap = 'gray', interpolation = 'bicubic')
     plt.xticks([]), plt.yticks([])  
     plt.show()
+    plt.clf()
 
 
-    
-    
     binary = cv2.threshold(imageGray,235,255,cv2.THRESH_BINARY)[1]
+
+
+    #plt.clf()
+    plt.imshow(binary, cmap = 'gray', interpolation = 'bicubic')
+    plt.xticks([]), plt.yticks([]) 
+    #plt.savefig("C:\\Users\\Desktop\\temp\\output\\binary_op.jpg") 
+    plt.title("binary threshold image (235-255)")
+    plt.show()
+    plt.clf()
+
+    #-------------------------------------------------------------
     
+    binary_inv= cv2.threshold(imageGray,235,255,cv2.THRESH_BINARY_INV)[1]
+    plt.imshow(binary_inv, cmap = 'gray', interpolation = 'bicubic')
+    plt.xticks([]), plt.yticks([]) 
+    #plt.savefig("C:\\Users\\Desktop\\temp\\output\\binary_op.jpg") 
+    plt.title("binary threshold inverted image (235-255)")
+    plt.show()
+    plt.clf()
+
+    #-------------------------------------------------------------
+
     contours, hierarchy = cv2.findContours(binary,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     contourMap = {}
     finalContourList = []
-
     contourMap = filterContoursBySize(contours)
-    
-
-    
     finalContourList = list(contourMap.values())
     finalContourList.sort(key=lambda x:get_contour_precedence(x, binary.shape[1]))
-
-    
-
     pts_x=[]
     pts_y=[]
-
+    print("\n\n\n\n")
+    mod10=0
     for i in finalContourList:
-        
         for j in i:
             
+            printlist.append('('+str(j[0][0])+','+str(j[0][0])+')')
             pts_x.append(j[0][0])
             pts_y.append(-1*j[0][1])
-    
-    
+            
+                
+                
+    time.sleep(3)    
     plt.scatter(pts_x, pts_y) 
     plt.show() 
 
-
-    return finalContourList
-
-def filterContoursBySize(contours):
-    
-    contourMap = {}
-
-    for i in range(len(contours)):
+    it=0
+    for r in printlist:
+        it+=1
+        print(r,end=' ')
+        if it%10==0:
+            print("")
         
+
+    #print(finalContourList,end=' ')
+    
+    return finalContourList
+    
+def filterContoursBySize(contours):
+    contourMap = {}
+    for i in range(len(contours)):
         if cv2.contourArea(contours[i]) < 120000 and cv2.contourArea(contours[i]) > 4000:
-            
             epsilon = 0.0025*cv2.arcLength(contours[i], True)
             approximatedContour = cv2.approxPolyDP(contours[i], epsilon, True)
             contourMap[i] = approximatedContour
-
     return contourMap
-
-
-
-
-
-
-
